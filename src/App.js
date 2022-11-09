d5d6e3a25726814a1dc35eabcee8b5fd8a053cd9
Amit Sakharkar <amitsakharkar12345@gmail.com>
2022-11-09 22:06:03 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Questionare from './screens/index';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Questionare />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/screens/index.js
@@ -0,0 +1,9 @@
+import react from 'react';
+
+const Questionaire = () => {  
+	return (
+		<h1>screens displyed here</h1>
+  );
+}
+
+export default Questionaire;
